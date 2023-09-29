b79a6bb06190e4267fac780d64d51179e6539bad
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-28 21:24:16 -0400

    refactor via module; test via chai&mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 // fetch breed details from web
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (body === '[]') {
-    console.log('Error: breed name is not found');
-    return;
-  }
-  // console.log(typeof body); ==> string
-  const data = JSON.parse(body);
-  // console.log(typeof body); ==> array
-  console.log(`Here's the description of ${breed}:\n`, data[0]);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (body === '[]') {
+      callback('Breed name is not found', null);
+    } else {
+      // console.log(typeof body); ==> string
+      const data = JSON.parse(body);
+      // console.log(typeof body); ==> array
+      // console.log(`Here's the description of ${breed}:\n`, data[0]);
 
-});
+      callback(null, data[0]['description']); // use callback to return data for async
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
